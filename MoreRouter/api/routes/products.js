85982acaec53f3ed70c7591b879ecd449d0c4081
manhph <phimanh2905@gmail.com>
2018-10-10 22:51:37 +0700

    update product

--- MoreRouter/api/routes/products.js
+++ MoreRouter/api/routes/products.js
@@ -1,16 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Product = require('../models/product');
+const Product = require("../models/product");
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handling get request to /products'
-    });
+router.get("/", (req, res, next) => {
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -23,12 +32,12 @@ router.post('/', (req, res, next) => {
         })
         .catch(err => console.log(err));
     res.status(201).json({
-        message: 'handling post request to /products',
+        message: "handling post request to /products",
         createdProduct: product
     });
 });
 
-router.get('/:productId', (req, res, next) => {
+router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
         .exec()
@@ -44,18 +53,28 @@ router.get('/:productId', (req, res, next) => {
         });
 });
 
-router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'update product'
+router.patch("/:productId", (req, res, next) => {
+    const id = res.params.productId;
+    Product.update({
+        _id: id
     });
 });
 
-router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'delete product'
-    });
+router.delete("/:productId", (req, res, next) => {
+    const id = res.params.productId;
+    Product.remove({
+            _id: id
+        })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
-
-
 
 module.exports = router;
